5f96424c367f7e3fed95e3a72b0fff9f45b55977
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-26 10:36:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   words_array = sentence.split(", ")
   new_array = []
-  words_array.each |word| do
+  words_array.each do |word|
     new_array << word.reverse
   end
   new_sentence = new_array.join
